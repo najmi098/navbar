f0da3d8214f936fed5e26faf99f82389a8fad7c7
najmi098 <37798239+najmi098@users.noreply.github.com>
2019-07-10 17:34:56 +0700

    Update juke.js

--- js/juke.js
+++ js/juke.js
@@ -1,6 +1,6 @@
 var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
-console.log("Kucing Firaun");
+console.log("Kucing Firaun Anaknya Bambang");
 for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function() {
   this.classList.toggle("active");
